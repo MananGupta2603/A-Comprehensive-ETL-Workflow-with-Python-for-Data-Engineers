11606f46ed135edc1b7eaf27a7eb69fd794cbb78
MananGupta2603 <mananngupta.official@gmail.com>
2024-12-21 09:59:34 +0530

    new files

--- logs/logs.py
+++
@@ -1,8 +0,0 @@
-
-import logging
-
-logging.basicConfig(filename='A-Comprehensive-ETL-Workflow-with-Python-for-Data-Engineers/logs/log_file.txt', 
-                    level=logging.INFO, 
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
-logging.info('ETL process started')
